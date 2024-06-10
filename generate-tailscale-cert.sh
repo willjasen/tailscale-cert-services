91117c77cd3834a3ccbf7b9b71a90bf22892886d
willjasen <brandon@willjasen.com>
2024-06-10 18:39:01 -0400

    change cron schedule

--- generate-tailscale-cert.sh
+++ generate-tailscale-cert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## This script generates a certificate from tailscale into $CERT_DIR
-## and sets up a cron job to renew it once per month
+## and sets up a cron job to renew it on the first of the month at 4 am
 
 source ./functions.sh;
 
@@ -15,7 +15,7 @@ is_jq_installed;
 CERT_DIR=/etc/ssl/tailscale;
 CERT_SCRIPT=/opt/generate-tailscale-cert.sh;
 THIS_SCRIPT_PATH=$(realpath "$0");
-CRON_JOB="0 3 1 * * $CERT_SCRIPT";
+CRON_JOB="0 4 1 * * $CERT_SCRIPT";
 CERT_NAME="$(tailscale status --json | jq '.Self.DNSName | .[:-1]' -r)";
 USER_FOR_PERMISSION=willjasen;
 
