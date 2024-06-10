dbf16fbe5663dc90347de4d75722a944ceb50ea2
willjasen <brandon@willjasen.com>
2024-06-10 13:28:44 -0400

    initial commit

---
+++ syncthing-cert.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Install jq if not installed
+if ! command -v jq &> /dev/null; then
+    echo "jq is not installed - installing...";
+    apt update -qq; apt install jq -y;
+else
+    echo "jq is installed, continuing...";
+fi;
+
+CONFIG_FILE="~/.config/syncthing/config.xml";
+CERT_NAME="$(tailscale status --json | jq '.Self.DNSName | .[:-1]' -r)";
+CERT_PATH="/etc/ssl/tailscale/"$CERT_NAME;
+
+# Lines to be added
+tls_cert_file="<tlsCertFile>"$CERT_PATH".crt</tlsCertFile>";
+tls_key_file="<tlsKeyFile>"$CERT_PATH".key</tlsKeyFile>";
+
+# Use sed to insert the lines after the <gui> tag
+sed -i "/<gui>/a \\
+    $tls_cert_file\\
+    $tls_key_file" "$CONFIG_FILE";
+
+echo "Tailscale TLS certificate and key paths added to Syncthing config.";
