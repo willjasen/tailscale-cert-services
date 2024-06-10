d5b3b7dbe53e5fc99c4f90487aa3d5cf3bba66b9
willjasen <brandon@willjasen.com>
2024-06-10 13:13:10 -0400

    set certificate permissions for a particular user

--- generate-tailscale-cert.sh
+++ generate-tailscale-cert.sh
@@ -25,11 +25,15 @@ CERT_SCRIPT=/opt/generate-tailscale-cert.sh;
 THIS_SCRIPT_PATH=$(realpath "$0");
 CRON_JOB="0 3 1 * * $CERT_SCRIPT";
 CERT_NAME="$(tailscale status --json | jq '.Self.DNSName | .[:-1]' -r)";
+USER_FOR_PERMISSION=willjasen;
 
-# Generate certificate
+# Generate Tailscale certificate
+# Access is given to the 
 mkdir -p $CERT_DIR;
 cd $CERT_DIR;
 tailscale cert "${CERT_NAME}";
+chown $USER_FOR_PERMISSION:$USER_FOR_PERMISSION $CERT_NAME".crt";
+chown $USER_FOR_PERMISSION:$USER_FOR_PERMISSION $CERT_NAME".key";
 
 # Setup cron schedule
 # This cron schedule happens at 3:00 am on the first of every month
