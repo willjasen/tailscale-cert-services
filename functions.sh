fb4204a4813fb48714068431e8cd735781004dc3
willjasen <brandon@willjasen.com>
2024-06-10 14:36:27 -0400

    add syncthing function

--- functions.sh
+++ functions.sh
@@ -21,3 +21,13 @@ if_jq () {
     echo "jq is installed, continuing...";
   fi;
 };
+
+# Exit if Syncthing is not installed already
+is_syncthing_installed () {
+  if ! command -v syncthing &> /dev/null; then
+    echo "Syncthing is not installed - exiting script.";
+    exit 1;
+  else
+    echo "Syncthing is installed, continuing...";
+  fi;
+};
