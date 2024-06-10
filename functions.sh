13079bf1b90cdb109569c94c6c9722a4bb34b3b0
willjasen <brandon@willjasen.com>
2024-06-10 14:31:51 -0400

    initial commit

---
+++ functions.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+### Functions are within here
+
+# Exit if Tailscale is not installed already
+is_tailscale_installed () {  
+  if ! command -v tailscale &> /dev/null; then
+    echo "Tailscale is not installed - exiting script.";
+    exit 1;
+  else
+    echo "Tailscale is installed, continuing...";
+  fi;
+};
+
+# Install jq if not installed
+if_jq () {
+  if ! command -v jq &> /dev/null; then
+    echo "jq is not installed - installing...";
+    apt update -qq; apt install jq -y;
+  else
+    echo "jq is installed, continuing...";
+  fi;
+};
