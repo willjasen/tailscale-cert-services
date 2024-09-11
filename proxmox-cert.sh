d553c0bb421025fb4a98d7e9d26899efc760e6e7
willjasen <brandon@willjasen.com>
2024-09-10 20:04:40 -0400

    fixing

--- proxmox-cert.sh
+++ proxmox-cert.sh
@@ -3,19 +3,13 @@
 ### This script updates Proxmox to use a Tailscale certificate
 ### This is easy to do as the .crt/.key can be used by Proxmox without changing format
 
-source ./functions.sh
-
-# Exit if Tailscale is not installed already
-is_tailscale_installed;
+source ./functions.sh;
 
 # Exit if Proxmox is not installed already
 is_proxmox_installed;
-
-# Install jq if not installed
-is_jq_installed;
 
 # Run main script
 ./generate-tailscale-cert.sh
 
 # Set the certificate
-pvenode cert set "${CERT_NAME}.crt" "${CERT_NAME}.key" --force --restart
+pvenode cert set "/etc/ssl/tailscale/${CERT_NAME}.crt" "/etc/ssl/tailscale/${CERT_NAME}.key" --force --restart
